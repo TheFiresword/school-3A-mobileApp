7179d61a9a7ed10656167bfe570b42b3b72e7c4d
rtas-vadum <agrand630@gmail.com>
2023-06-18 12:34:07 +0200

    quelques routes

--- backend/routes/rescuersRoutes.js
+++ backend/routes/rescuersRoutes.js
@@ -23,12 +23,30 @@ router.get('/', async (req, res)=>{
 
 // Fonction pour récupérer la liste des secouristes disponibles
 router.get('/available', (req, res)=>{
-
+    const connection = req.connection;
+    connection.query('SELECT * from rescuers WHERE DISPONIBILITY = 1', (err, results, fields) =>{
+        if(err){
+            console.error('Erreur lors de l\'exécution de la requête', err);
+            res.status(500).send('Erreur de serveur');
+        }
+        else{
+            res.json(results);
+        }
+    } )
 })
 
 // Fonction pour récupérer un secouriste en particulier
 router.get('/:id', (req, res)=>{
-
+    const connection = req.connection;
+    connection.query('SELECT * from rescuers WHERE id='+ mysql.escape(req.params.id), (err, results, fields) =>{
+        if(err){
+            console.error('Erreur lors de l\'exécution de la requête', err);
+            res.status(500).send('Erreur de serveur');
+        }
+        else{
+            res.json(results);
+        }
+    } )
 })
 
 // Fonction pour ajouter un secouriste dans la base

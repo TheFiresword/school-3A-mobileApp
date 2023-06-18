583b1365ccd828be293b093dca837cdc86f3bbe6
GGravys-S <simeon.gravis@insa-cvl.fr>
2023-06-18 06:38:20 +0200

    Added the big red button

--- app/src/main/java/com/example/sstla/MainActivity.kt
+++ app/src/main/java/com/example/sstla/MainActivity.kt
@@ -4,17 +4,25 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.content.Intent
+import android.widget.ImageView
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val myButton = findViewById<Button>(R.id.helpButton)    //Bouton "demander de l'aide"
+        //val myButton = findViewById<Button>(R.id.helpButton)    //Bouton "demander de l'aide"
+        val myhelpbutton = findViewById<ImageView>(R.id.helpImage)
 
-        myButton.setOnClickListener {                                               //Quand on clique sur le bouton AIDE
+        /*myButton.setOnClickListener {                                               //Quand on clique sur le bouton AIDE
             val intent = Intent(this, EnterLocationActivity::class.java)       //Se diriger vers l'activité "Entrer salle"
             startActivity(intent)
+        }*/
+
+        myhelpbutton.setOnClickListener {                                           //Quand on clique sur le bonton rouge
+            val intent = Intent(this, EnterLocationActivity::class.java)
+            startActivity(intent)                                                          //se diriger vers l'activité EnterLocation
         }
 
     }

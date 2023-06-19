9be865c3c2dde8c7af27ebff0414357ff19d6d96
rtas-vadum <agrand630@gmail.com>
2023-06-19 14:41:17 +0200

    tentative pour firebase (je crois le pire...)

---
+++ app/src/main/java/insa/etudiant/sstou/WaitingForHelp.kt
@@ -0,0 +1,31 @@
+package insa.etudiant.sstou
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import insa.etudiant.sstou.R
+
+class WaitingForHelp : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_waiting_for_help)
+
+        val location = intent.getStringExtra("location")
+        val waitingSentence1 = getString(R.string.waitingSentence1)
+        val waitingSentence2 = getString(R.string.waitingSentence2)
+
+        val waitingText = findViewById<TextView>(R.id.waitingText)
+        waitingText.text = waitingSentence1 + location + waitingSentence2
+
+        val retButton = findViewById<Button>(R.id.button2)     //Bouton de retour vers la page d'accueil.
+        retButton.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+
+
+    }
+}

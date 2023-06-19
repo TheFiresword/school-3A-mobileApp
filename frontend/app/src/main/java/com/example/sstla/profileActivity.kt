29cd1d2ce6899f688a32ec6fa286b43410a24cee
TheFiresword <109469684+TheFiresword@users.noreply.github.com>
2023-06-19 14:54:11 +0200

    Patch frontend

---
+++ frontend/app/src/main/java/com/example/sstla/profileActivity.kt
@@ -0,0 +1,35 @@
+package com.example.sstla
+
+import android.content.Intent
+import android.icu.text.CaseMap.Title
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+
+class profileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profile)
+
+        val userTitle = intent.getStringExtra("userTitle")
+        val Title = findViewById<TextView>(R.id.SSTProfile_Title)
+
+        Title.setText(userTitle)
+
+        //Remplir les valeurs par défault avec celle de la base de données.
+
+        val Confirmation_button = findViewById<Button>(R.id.button_confirmation)
+
+        Confirmation_button.setOnClickListener {
+
+            //Insérer Comparaison ici
+            //Insérer requête SQL ici
+
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        //Récupérer les données
+    }
+}

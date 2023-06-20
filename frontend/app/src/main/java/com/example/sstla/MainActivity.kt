9397726cf7d6a37353a3a27787e5985212edf4bf
Yann-j3 <gaudichaudyann@gmail.com>
2023-06-20 08:09:23 +0200

    now send sms

--- frontend/app/src/main/java/com/example/sstla/MainActivity.kt
+++ frontend/app/src/main/java/com/example/sstla/MainActivity.kt
@@ -16,7 +16,6 @@ class MainActivity : AppCompatActivity() {
         val myhelpbutton = findViewById<ImageView>(R.id.HelpButton)
         val myparametersbutton = findViewById<Button>(R.id.Params_button)
 
-
         myhelpbutton.setOnClickListener {                                           //Quand on clique sur le bonton rouge
             val intent = Intent(this, EnterLocationActivity::class.java)
             startActivity(intent)                                                          //se diriger vers l'activité EnterLocation
